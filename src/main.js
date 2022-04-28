0b96c7f9d4e1759decb9d2f69e36a1a3283a3ed6
GiSeong Lee <cj8928@gmail.com>
2022-04-28 23:03:00 +0900

    start capstone

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron') 
+const path = require('path') 
+function createWindow () { 
+  const win = new BrowserWindow({ 
+    width: 1920, 
+    height: 1080, 
+    webPreferences: { 
+      nodeIntegration: true,
+      contextIsolation : false
+    } 
+  }) 
+  win.loadURL("http://localhost:3000")
+} 
+app.whenReady().then(() => { 
+  createWindow() 
+}) 
+app.on('window-all-closed', function () { 
+  if (process.platform !== 'darwin') app.quit() 
+})
